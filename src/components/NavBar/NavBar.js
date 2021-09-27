ac00a3ac2e0efa51b0c8339f8b13805292c7b745
unknown <jesteradduru@gmail.com>
2021-09-27 21:22:26 +0800

    Made the navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
+import Logo from "../../assets/img/logo.png"
+
+const NavBar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar light expand="md" className="text-purple">
+        <NavbarBrand href="/">
+          <img className="img-fluid" src={Logo} width="40" height="40" class="d-inline-block align-top" alt="logo"/>
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ms-auto" navbar>
+            <NavItem>
+              <NavLink href="#">About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Works</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Contacts</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ src/containers/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Container } from 'reactstrap';
+import NavBar from '../../components/NavBar/NavBar';
+
+function App() {
+  return (
+    <Container className="themed-container">
+      <NavBar />
+    </Container>
+  );
+}
+
+export default App;
